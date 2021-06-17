a59824b33ab45872727e40292e7d4fb0c1f58563
Mayank Sharma <mayankece155@gmail.com>
2021-06-17 22:00:03 +0530

    get-all and find-one users

--- controller/company.controller.js
+++ controller/company.controller.js
@@ -16,6 +16,28 @@ module.exports = {
             return res.response(error).code(500);
         }
     },
-  
+    async find(req, reply) {
+
+        try {
+            var company = await Company.find().exec();
+            return reply.response(company);
+        } catch (error) {
+            return reply.response(error).code(500);
+        }
+    },
+    async findOne(req, reply) {
+        if (!req.params.id) {
+            return reply({ err: 'id is required param' }).code(400);
+        }
+
+        try {
+            var company = await Company.findById(req.params.id).exec();
+            return reply.response(company);
+        } catch (error) {
+            return reply.response(error).code(500);
+        }
+
+    },
+   
 
 };
