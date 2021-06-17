8f66cea2641b0a4bbfcf4b70b4599064ce0600c4
Mayank Sharma <mayankece155@gmail.com>
2021-06-17 21:54:24 +0530

    create users to database

--- controller/company.controller.js
+++ controller/company.controller.js
@@ -3,6 +3,19 @@ const Company = db.company
 
 module.exports = {
 
-    
+    async create(req, reply) {
+        if (!req.payload.name) {
+            return reply({ er: 'name is required field' }).code(400);
+        }
+
+        try {
+            var company = new Company(req.payload);
+            var result = await company.save();
+            return reply.response(result);
+        } catch (error) {
+            return res.response(error).code(500);
+        }
+    },
+  
 
 };
