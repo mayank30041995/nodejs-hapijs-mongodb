1bf49ea9b96e54116c7a0cec3a55d06b72905583
Mayank Sharma <mayankece155@gmail.com>
2021-06-17 22:04:10 +0530

    update user

--- controller/company.controller.js
+++ controller/company.controller.js
@@ -38,6 +38,28 @@ module.exports = {
         }
 
     },
-   
+    async update(req, reply) {
+        if (!req.params.id) {
+            return reply({ err: 'id is required param' }).code(400);
+        }
+        let attributes = {};
+
+        if (req.payload.name) {
+            attributes.name = req.payload.name;
+        }
+        if (req.payload.city) {
+            attributes.city = req.payload.city;
+        }
+        if (req.payload.address) {
+            attributes.address = req.payload.address;
+        }
+        try {
+            var result = await Company.findByIdAndUpdate(req.params.id, req.payload, { new: true });
+            return reply.response(result);
+        } catch (error) {
+            return reply.response(error).code(500);
+        }
+
+    }
 
 };
